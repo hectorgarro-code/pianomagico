657f9fd307fd24bd058c35189b5577861a00481e
hectorgarro-code <hectorgarro@gmail.com>
2026-09-12 13:03:00 -0300

    Configurada autodeteccion de base de datos local y Hostinger en dbconfig.php

--- backend/dbconfig.php
+++ backend/dbconfig.php
@@ -1,16 +1,24 @@
 <?php
 // backend/dbconfig.php
-$host = "localhost";
-$db_name = "pianomagicoweb";
-$username = "root";
-$password = "";
+$is_production = isset($_SERVER['HTTP_HOST']) && strpos($_SERVER['HTTP_HOST'], 'localhost') === false && strpos($_SERVER['HTTP_HOST'], '127.0.0.1') === false;
+
+if ($is_production) {
+    $host = "localhost";
+    $db_name = "u803496046_pianomagicoweb";
+    $username = "u803496046_pianomagicoweb";
+    $password = "6A8edb2013";
+} else {
+    $host = "localhost";
+    $db_name = "pianomagicoweb";
+    $username = "root";
+    $password = "";
+}
 
 try {
-    $conn = new PDO("mysql:host=" . $host . ";dbname=" . $db_name, $username, $password);
+    $conn = new PDO("mysql:host=" . $host . ";dbname=" . $db_name . ";charset=utf8", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conn->exec("set names utf8");
 } catch(PDOException $exception) {
-    // Error will be caught by the calling script
     $conn = null;
 }
 ?>
